a3c486c6dd7cc8fd171083e415e2052542b47031
EXEQUIELKENT <130205735+EXEQUIELKENT@users.noreply.github.com>
2026-01-12 21:30:14 +0800

    update

--- lgu-portal/public/db.php
+++ lgu-portal/public/db.php
@@ -2,9 +2,9 @@
 // config/db.php
 
 $DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASS = "";          // default XAMPP password
-$DB_NAME = "lgu_portal";
+$DB_USER = "cimm_root";
+$DB_PASS = "xqjzFor8gi!e54@3";          // default XAMPP password
+$DB_NAME = "cimm_LGU";
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
