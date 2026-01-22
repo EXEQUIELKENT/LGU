ab3354c21867dd1a1414f94816fdd06fd9a1c2e8
EXEQUIELKENT <130205735+EXEQUIELKENT@users.noreply.github.com>
2026-01-22 16:23:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 // Directly include the home page (no redirect)
 //require_once __DIR__ . '/lgu-portal/public/login.php';
-require_once __DIR__ . '/lgu-portal/public/citizendash.php';
+require_once __DIR__ . '/lgu-portal/public/citizencimm.php';
